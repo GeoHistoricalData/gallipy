1b735b08b55a815876150052f51c63cac34510bb
HueyNemud <bertranddumenieu@gmail.com>
2019-05-25 01:50:00 +0200

    Reworked & linted class Ark

--- gallipy/ark.py
+++ gallipy/ark.py
@@ -1,136 +1,243 @@
-from rfc3987 import parse as rfcparse
+"""
+Gallipy - Python wrapper for the Gallica APIs
+Copyright (C) 2019  Bertrand Dumenieu
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+https://github.com/GeoHistoricalData/gallipy
+"""
+import rfc3987
 from lark import Transformer, Lark
 from lark.exceptions import ParseError, UnexpectedCharacters
 from .monadic import Either, Left
-import traceback
 
 __all__ = ['Ark', 'ArkParsingError']
 
 # ARKID structure :  ark:/NAAN/Name[Qualifier]
 _GRAMMAR = """
-    arkid: _ark_items _COLON _SLASH naan _SLASH name [ _SLASH qualifier ]
-    naan: STRING
-    name: STRING
-    qualifier: STRING
+    arkid: _SCHEME _COLON _SLASH naan _SLASH name [_SLASH qualifier]
+    naan: SAFESTRING
+    name: SAFESTRING
+    qualifier: SAFESTRING
     _SLASH: "/"
     _COLON: ":"
-    _ark_items: "ark"i
-    STRING: /[^:\/\?#\[\]@]+/
+    _SCHEME: "ark"i
+    SAFESTRING: /[^@:\\/#\\[\\]\\?]+/
 """
+
+_ARKID_SCHEME = "ark"
 
 
 class Ark:
+    """Object representation of an Archival Resource Key.
 
-    _fields = ["scheme", "authority", "naan", "name", "qualifier"]
-    _arkidfields = ["scheme", "naan", "name", "qualifier"]
+    An object representation of an Archival Resource Key, which can be
+    either a full ARK URL or an ARK ID, following the structure
+    [urlscheme://authority/]ark:/naan/name[/qualifier].
 
-    def __init__(self, **arkinfo):
-        # If authority is set then scheme must be set
-        if arkinfo.get("authority") and not arkinfo.get("scheme"):
-            msg = "Field 'scheme' and 'authority' \
-            must be both set if one of them is set."
-            raise ValueError(msg)
+    Args:
+        naan (str): Name Assigning Authority Number.
+        name (str): ARK ID name.
+        qualifier (:obj:`str`, optional): ARK qualifier.
+        scheme (:obj:`str`, optional): The scheme of the ARK.
+            Can be any http scheme, or 'ark'. Defaults to 'ark'.
+        authority (:obj:`str`, optional): The naming authority, e.g. gallica.bnf.fr.
+            If authority is set them scheme must be a http scheme.
+    Attributes:
+        ark_parts (dict): A dictionary of ark parts. Must contain at least keys
+            'naan' and 'name'. If key 'authority' is set, key 'scheme' must be
+            different from 'ark'.
+    Raises:
+        ValueError: If parameters naan or name are undefined, or if scheme='ark'
+            and authority is set.
+    """
+
+    def __init__(self, **ark_parts):
+        valid_keys = ["scheme", "authority", "naan", "name", "qualifier"]
+        parts = {key: ark_parts.get(key) for key in valid_keys}
+        # Scheme must be ark if authority is unset. 'ark' is also scheme's
+        # default value.
+        if not(parts["scheme"] and parts["authority"]):
+            parts["scheme"] = 'ark'
+
         # naan and name are required
-        if not(arkinfo.get("naan") and arkinfo.get("name")):
-            msg = "'naan' and 'name' are required fields."
+        if not parts["naan"]:
+            raise ValueError("Parameter naan is required.")
+        if not parts["name"]:
+            raise ValueError("Parameter name is required.")
+        # scheme cannot be 'ark' if authority is set
+        if parts["authority"] and parts["scheme"] == _ARKID_SCHEME:
+            msg = """
+            Cannot create an Ark object with parts {}
+            Scheme cannot be '{}' if authority is set.
+            """.format(str(parts), _ARKID_SCHEME)
             raise ValueError(msg)
-        self._ark_items = {key: arkinfo.get(key) for key in self._fields}
-    
-    def copy():
-        return Ark(self._ark_items)
+
+        self._ark_parts = parts
+
+    def copy(self):
+        """Copy constructor.
+
+        Returns:
+            Ark: A copy of self
+        """
+        return Ark(**self._ark_parts)
 
     @property
     def scheme(self):
-        return self._ark_items.get("scheme")
+        """The scheme of this Ark.
+
+        Returns:
+            str: The scheme of self.
+        """
+        return self._ark_parts.get("scheme")
 
     @property
     def authority(self):
-        return self._ark_items.get("authority")
+        """The authority of this Ark.
+
+        Returns:
+            str: The authority of self.
+        """
+        return self._ark_parts.get("authority")
 
     @property
     def naan(self):
-        return self._ark_items.get("naan")
+        """The naming authority assigning number of this Ark.
+
+        Returns:
+            str: The naming assigning number of self.
+        """
+        return self._ark_parts.get("naan")
 
     @property
     def name(self):
-        return self._ark_items.get("name")
+        """The name of this Ark.
+
+        Returns:
+            str: The name of self.
+        """
+        return self._ark_parts.get("name")
 
     @property
     def qualifier(self):
-        return self._ark_items.get("qualifier")
+        """The qualifier of this Ark.
+
+        Returns:
+            str: The qualifier of self.
+        """
+        return self._ark_parts.get("qualifier")
 
     @property
     def arkid(self):
-        if self._ark_items.get('scheme') == 'ark':
+        """The ARK ID of this Ark.
+
+        If this Ark is an URL, extract the ARK ID nester inside as a new Ark
+        object. Otherwise, return self.
+
+        Returns:
+            Ark: If self is a full ARK URL, a new Ark object representing the
+            ARK ID nested in self. Otherwise, return self.
+        """
+        if self.is_arkid():
             return self
-        else:
-            infos = self._ark_items.copy()
-            # Change the scheme from whatever (e.g. http) to ark
-            infos['scheme'] = 'ark' 
-            items = {key: infos.get(key) for key in self._arkidfields}
-            return Ark(**items)
+        parts = self._ark_parts.copy()
+        parts['scheme'] = 'ark'
+        del parts['authority']
+        return Ark(**parts)
+
+    def is_arkid(self):
+        """The ARK ID of this Ark.
+        Returns:
+            bool: True if self is an ARK ID, False if self is a full ARK URL.
+        """
+        return self.scheme == _ARKID_SCHEME
 
     @staticmethod
-    def parse(arkstr):
+    def parse(ark_str):
+        """Parse an ARK URL or an ARK ID string into an Ark oject
+            Args:
+                ark_str (str): The string to parse.
+
+            Returns:
+                Ark: The parsed ARK.
+        """
         try:
-            arkdict = rfcparse(arkstr, rule="URI")  # Ensure ark is a URI
-            arkid_parser = Lark(_GRAMMAR, start='arkid')
-            if arkdict["scheme"] != 'ark':
-                # If ArkID is nested inside an URL, extract it
-                arkid = arkdict["path"].lstrip("/")
-                if not arkdict["authority"]:  # The NMAH is required
-                    msg = 'Name mapping authority cannot be null.'
-                    raise ArkParsingError(msg, arkstr)
+            parts = rfc3987.parse(ark_str, rule="URI")  # Ensure ark is a URI
+            parser = Lark(_GRAMMAR, start='arkid')
+
+            # Extract an ARK ID from ark_str if ark_str is a full ARK URL.
+            if parts["scheme"] != _ARKID_SCHEME:
+                arkid_str = parts["path"].lstrip("/")
+                if not parts["authority"]:  # NMA is required
+                    msg = 'Name Mapping Authority cannot be null.'
+                    raise ArkParsingError(msg, ark_str)
             else:
-                arkid = arkstr
-            tree = arkid_parser.parse(arkid)
-            arkiddict = ArkIdTransformer().transform(tree)
-            final_dict = {**arkdict, **arkiddict}
-            return Either.pure(Ark(**final_dict))
-        except (ParseError, UnexpectedCharacters) as e:
-            return Left(ArkParsingError(str(e), arkstr))
-        except Exception as e:
-            return Left(e)
+                arkid_str = ark_str
 
-    def _str_ark_itemsid(self, arkiddict):
-        pattern = "{scheme}:/{naan}/{name}"
-        if arkiddict.get("qualifier"):
-            pattern += "/{qualifier}"
-        return pattern.format(**arkiddict)
+            tree = parser.parse(arkid_str)
+            ark_parts = ArkIdTransformer().transform(tree)
+            ark_parts.update(parts)
+            ark = Ark(**ark_parts)
+            return Either.pure(ark)
 
-    def _str_ark_itemsurl(self, arkurldict):
-        return "{scheme}://{authority}/{arkid}".format(**arkurldict)
+        except (TypeError, ValueError, ParseError, UnexpectedCharacters) as ex:
+            print(str(ex))
+            return Left(ArkParsingError(str(ex), ark_str))
 
     def __str__(self):
-        ark = self._str_ark_itemsid(self._ark_items)
-        if self._ark_items.get("authority"):
-            ark = self._str_ark_itemsurl({**self._ark_items, **{"arkid": ark}})
-        return ark
+        """Simple string representation of this Ark"""
+
+        pattern = "{scheme}://{authority}/" if not self.is_arkid() else ""
+        pattern += _ARKID_SCHEME+":/{naan}/{name}"
+        pattern += "/{qualifier}" if self.qualifier else ""
+        return pattern.format(**self._ark_parts)
 
     def __repr__(self):
-        return str(self._ark_items)
+        """Simple string representation of the parts composing this Ark"""
+        return str(self._ark_parts)
 
 
 class ArkParsingError(ValueError):
-
+    """A simple parsing exceptions for Arks."""
     def __init__(self, message, arkstr):
-        template = """
+        string = """
             Parsing error, ARK '{}' is invalid. See details below.
             {}
-        """
-        super(ValueError, self).__init__(template.format(arkstr, message))
+        """.format(arkstr, message)
+        super().__init__(string)
 
 
 class ArkIdTransformer(Transformer):
+    """A tree transformer for Ark parsing."""
 
-    def naan(self, item):
+    @staticmethod
+    def naan(item):
+        """Get naan item from the naan TreeNode."""
         return "naan", str(item[-1])
 
-    def name(self, item):
+    @staticmethod
+    def name(item):
+        """Get name item from the naan TreeNode."""
         return "name", str(item[-1])
 
-    def qualifier(self, item):
+    @staticmethod
+    def qualifier(item):
+        """Get qualifier item from the naan TreeNode."""
         return "qualifier", str(item[-1])
 
-    def arkid(self, items):
+    @staticmethod
+    def arkid(items):
+        """Get arkid item from the naan TreeNode."""
         return dict(items)
--- tests/test_ark.py
+++ tests/test_ark.py
@@ -1,45 +1,84 @@
+"""
+Gallipy - Python wrapper for the Gallica APIs
+Copyright (C) 2019  Bertrand Dumenieu
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+https://github.com/GeoHistoricalData/gallipy
+"""
 import pytest
 from gallipy.monadic import Right, Left
 from gallipy import Ark
 
-# ---
-# ARK Parser 
-# ---
-
-ark_test_cases = [
-  (None, Left),
-  ('', Left),
-  ('gallica.bnf.fr/ark:/12148/bpt6k5619759j', Left),  # Scheme is missing
-  ('http:///ark:/12148/bpt6k5619759j', Left),  # NMAH is missing
-  ('http://gallica.bnf.fr/', Left),  # ARKId is missing
-  ('You can\'t see me', Left),  # JOHN CENA!
-  ('https://gallica.bnf.fr/ark:/12148/bpt6k5619759j', Right), # Valid ARK
-  ('https://gallica.bnf.fr/ark:/12148/bpt6k5619759j/f1n10.pdf', Right)  # Valid ARK with qualifier
+TEST_CASES = [
+    ({"naan":"naan", "name":"name", "authority":"authority"}, ValueError),
+    ({"scheme":"scheme", "authority":"authority"}, ValueError),
 ]
 
-@pytest.mark.parametrize("arkstring,expected", ark_test_cases)
-def test_ark(arkstring, expected):
-  assert type(Ark.parse(arkstring)) == expected
+@pytest.mark.parametrize("test,expected", TEST_CASES)
+def test_constructor_nok(test, expected):
+    """Test constructor failures."""
+    with pytest.raises(expected):
+        Ark(**test)
+
+TEST_CASES = [
+    ({"naan":"n", "name":"n", "scheme":"s", "qualifier":"q"}),
+    ({"scheme":"s", "authority":"a", "naan":"n", "name":"n", "qualifier":"q"}),
+]
+@pytest.mark.parametrize("test", TEST_CASES)
+def test_constructor_ok(test):
+    """Test constructor success."""
+    print(Ark(**test))
+
+
+TEST_CASES = [
+    (None, Left),
+    ('', Left),
+    ('gallica.bnf.fr/ark:/12148/bpt6k5619759j', Left),  # Scheme is missing
+    ('http:///ark:/12148/bpt6k5619759j', Left),  # NMAH is missing
+    ('http://gallica.bnf.fr/', Left),  # ARKId is missing
+    ('You can\'t see me', Left),  # JOHN CENA!
+    ('https://gallica.bnf.fr/ark:/12148/bpt6k5619759j', Right), # Valid ARK
+    ('https://gallica.bnf.fr/ark:/12148/bpt6k5619759j/f1n10.pdf', Right),
+    ('12148/bpt6k5619759j', Left),  # Scheme is missing
+    ('ark://12148', Left),  # Naan is missing
+    ('ark:/12148/', Left),  # Name is missing
+    ('You can\'t see me', Left),  # JOHN CENA!
+    ('/ark:/12148/bpt6k5619759j', Left),  # Leading slash
+    ('ark:/12148/bpt6k5619759j/', Left),  # Trailing slash
+    ('ark:/12148/bpt6k5619759j', Right),  # Valid ARK id
+    ('ark:/12148/bpt6k?@:5619759j', Left),  # Invalid chars
+    ('ark:/12148/bpt6k5619759j/f1n10.pdf', Right),  # Valid ARK id with qualifier
+]
+
+@pytest.mark.parametrize("test,expected", TEST_CASES)
+def test_ark(test, expected):
+    """Test ARK parsing."""
+    assert isinstance(Ark.parse(test), expected)
 
 def test_ark_extract_arkid():
-  ark = 'https://gallica.bnf.fr/ark:/12148/bpt6k5619759j'
-  arkid = 'ark:/12148/bpt6k5619759j'
-  parsed = Ark.parse(ark)
-  assert str(parsed.value.arkid) == arkid
+    """Test ARK ID extraction from ARK URL."""
+    ark = 'https://gallica.bnf.fr/ark:/12148/bpt6k5619759j'
+    arkid = 'ark:/12148/bpt6k5619759j'
+    parsed = Ark.parse(ark)
+    assert str(parsed.value.arkid) == arkid
 
-arkid_test_cases = [
-  (None, Left),
-  ('', Left),
-  ('12148/bpt6k5619759j', Left),  # Scheme is missing  
-  ('ark://12148', Left),  # Naan is missing
-  ('ark:/12148/', Left),  # Name is missing
-  ('You can\'t see me', Left),  # JOHN CENA!
-  ('/ark:/12148/bpt6k5619759j', Left),  # Leading slash
-  ('ark:/12148/bpt6k5619759j/', Left),  # Trailing slash
-  ('ark:/12148/bpt6k5619759j', Right),  # Valid ARK id
-  ('ark:/12148/bpt6k5619759j/f1n10.pdf', Right)  # Valid ARK id with qualifier
+TEST_CASES = [
+    ('ark:/12148/bpt6k5619759j/f1n10.pdf'),  # Valid ARK id with qualifier
 ]
 
-@pytest.mark.parametrize("arkidstring,expected", arkid_test_cases)
-def test_arkid(arkidstring, expected):
-    assert type(Ark.parse(arkidstring)) == expected
+@pytest.mark.parametrize("test", TEST_CASES)
+def test_copy(test):
+    """Test copy constructor."""
+    assert str(Ark.parse(test).value) == str(Ark.parse(test).value.copy())
