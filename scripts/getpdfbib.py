2885d7437fdb841a825161e0286bbecc53697595
HueyNemud <bertranddumenieu@gmail.com>
2019-06-28 16:55:26 +0200

    A script to download PDFs from Bibtex entries

---
+++ scripts/getpdfbib.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import bibtexparser, argparse
+import getpdf
+
+parser = argparse.ArgumentParser(description='A simple script to download the PDF version of an archival resource hosted by Gallica.')
+parser.add_argument('bibtex', type=str,
+                    help='A path to a Bibtex file or a string containing a set of Bibtex entries.' )
+parser.add_argument('--key', type=str, default='eprint',
+                    help='Name of the Bibtex key containing the ARK.' )
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    with open(args.bibtex) as bibtex_file:
+        bib_database = bibtexparser.load(bibtex_file)
+        for idx, bib in enumerate(bib_database.entries):
+          bibkey = bib['ID']
+          ark = bib[args.key]
+          if bib['vues']:
+            vues = [int(s) for s in bib['vues'].split('--') if s.isdigit()]
+          else:
+            vues = [1,0]
+
+          print("Downloading entry {}: {} [{}]{}".format(idx+1, bibkey, ark, ', views '+bib['vues']))
+          getpdf.get_pdf(ark, bibkey+bib['vues']+'.pdf', vues[0], vues[1], True)         
