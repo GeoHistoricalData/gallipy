2c7ad1c30e3298316cdd102a1edefd6fb0d9de4b
Bertrand Duménieu <bertrand.dumenieu@ehess.fr>
2023-04-07 18:30:02 +0200

    All verbs implemented in Document API

--- gallipy/document.py
+++ gallipy/document.py
@@ -1,30 +1,22 @@
-# import routes
-# from helpers import sync_get
-# from dataclasses import dataclass
-# from bs4 import BeautifulSoup
-# from PIL import Image
-# from io import BytesIO
-# from ark import Ark
-# from parsers import (
-#     OAIRecordParser,
-#     PaginationParser,
-#     XmlParser,
-#     IssuesParser,
-#     ContentSearchParser,
-#     html2text,
-# )
-
-
-from typing import Any, Union
+from enum import Enum
+import io
+from typing import Any, Literal, Union
 from bs4 import BeautifulSoup
+from PIL import Image as PILImage
 import httpx
 from pydantic import BaseModel, validator
 
 from ark import Ark
 
-ENDPOINT_ISSUES = "https://gallica.bnf.fr/services/Issues"
-ENDPOINT_OAIRecord = "https://gallica.bnf.fr/services/OAIRecord"
-ENDPOINT_Pagination = "https://gallica.bnf.fr/services/Pagination"
+GALLICA = "https://gallica.bnf.fr"
+ENDPOINT_ISSUES = f"{GALLICA}/services/Issues"
+ENDPOINT_OAIRecord = f"{GALLICA}/services/OAIRecord"
+ENDPOINT_Pagination = f"{GALLICA}/services/Pagination"
+ENDPOINT_Image = GALLICA
+ENDPOINT_ContentSearch = f"{GALLICA}/services/ContentSearch"
+ENDPOINT_Toc = f"{GALLICA}/services/Toc"
+ENDPOINT_OCR = f"{GALLICA}/RequestDigitalElement"
+
 
 def copyparse(ark: Union[str, Ark]) -> Ark:
     if isinstance(ark, Ark):
@@ -35,6 +27,11 @@ def copyparse(ark: Union[str, Ark]) -> Ark:
         raise TypeError(f"{ark} is not an ARK.")
 
 
+class MarkupFormat(Enum):
+    HTML = None
+    XML = "lxml"
+
+
 class Service(BaseModel):
     ark: Ark
 
@@ -42,13 +39,19 @@ class Service(BaseModel):
     def init_ark(cls, ark: Union[str, Ark]):
         return copyparse(ark)
 
-    def fetch_xml(self, url, params, client: httpx.Client):
+    def fetch_markup(
+        self,
+        url,
+        params,
+        client: httpx.Client,
+        format: MarkupFormat = MarkupFormat.HTML,
+    ):
         r = client.get(url, params=params)
         r.raise_for_status()
-        return BeautifulSoup(r.text, features="xml")
+        return BeautifulSoup(r.text, features=format.value)
+
 
 class Issues(Service):
-
     @validator("ark", pre=True)
     def init_ark(cls, ark: Union[str, Ark]):
         ark = super().init_ark(ark)
@@ -57,160 +60,88 @@ class Issues(Service):
         return ark
 
     def get(self, client: httpx.Client, date: int = None):
-        p = {"ark": self.ark, "date": date}
-        return self.fetch_xml(ENDPOINT_ISSUES, p, client)
+        return self.fetch_markup(
+            ENDPOINT_ISSUES,
+            {"ark": self.ark, "date": date},
+            client,
+            format=MarkupFormat.XML,
+        )
+
 
 class OAIRecord(Service):
     def get(self, client: httpx.Client):
-        p = {"ark": self.ark.name}
-        return self.fetch_xml(ENDPOINT_OAIRecord, p, client)
+        return self.fetch_markup(
+            ENDPOINT_OAIRecord, {"ark": self.ark.name}, client, format=MarkupFormat.XML
+        )
+
 
 class Pagination(Service):
     def get(self, client: httpx.Client):
-        p = {"ark": self.ark.name}
-        return self.fetch_xml(ENDPOINT_Pagination, p, client)
+        return self.fetch_markup(
+            ENDPOINT_Pagination, {"ark": self.ark.name}, client, format=MarkupFormat.XML
+        )
 
 
-with httpx.Client() as c:
-    i = Issues(ark="ark:/12148/cb32798952c").get(c, date=1937)
+class ImageQualifier(Enum):
+    THUMBNAIL = "thumbnail"
+    LOWRES = "lowres"
+    MEDRES = "medres"
+    HIGHRES = "highres"
+
+
+class Image(Service):
+    def get(self, client: httpx.Client, qualifier: ImageQualifier):
+        url = f"{ENDPOINT_Image}/{self.ark}/{qualifier}"
+        r = client.get(url)
+        r.raise_for_status()
+        return PILImage.open(io.BytesIO(r.content))
+
+
+class ContentSearch(Service):
+    def get(
+        self,
+        client: httpx.Client,
+        query: str,
+        page: int = None,
+        startResult: int = None,
+    ):
+        params = {
+            "ark": self.ark.name,
+            "query": query,
+            "page": page,
+            "startResult": startResult,
+        }
+        params = {k: v for k, v in params.items() if v}
+        return self.fetch_markup(
+            ENDPOINT_ContentSearch, params, client, format=MarkupFormat.XML
+        )
+
+
+class Toc(Service):
+    def get(self, client: httpx.Client):
+        return self.fetch_markup(
+            ENDPOINT_Toc, {"ark": self.ark}, client=client, format=MarkupFormat.XML
+        )
+
+
+class Text(Service):
+    def get_plaintext(self, client: httpx.Client):
+        url = f"{GALLICA}/{self.ark}/texteBrut"
+        r = client.get(url)
+        r.raise_for_status()
+        return r.content
+
+    def get_ocrtext(self, client: httpx.Client, deb: int):
+        params = {"O": self.ark.name, "E": "ALTO", "Deb": deb}
+        return self.fetch_markup(
+            ENDPOINT_OCR, params=params, client=client, format=MarkupFormat.XML
+        )
+
+
+with httpx.Client(timeout=10) as c:
+    # i = Issues(ark="ark:/12148/cb32798952c").get(c, date=1937)
+    # print(i)
+    # i = OAIRecord(ark="ark:/12148/cb32798952c").get(c)
+    # print(i)
+    i = Text(ark="12148/bpt6k5619759j").get_ocrtext(c, deb=3)
     print(i)
-    i = OAIRecord(ark="ark:/12148/cb32798952c").get(c)
-    print(i)
-# @dataclass
-# class Document:
-
-#     ark: Ark
-
-#     def __init__(self, ark_):
-#         if isinstance(ark_, str):
-#             self.ark = Ark.parse(ark_)
-#         elif isinstance(ark_, Ark):
-#             self.ark = ark_
-#         else:
-#             raise ValueError(ark_, "is not a string or an Ark object.")
-
-#     def issues(self, date: int = None, raw: bool = False):
-#         if self.ark.qualifier == "date":
-#             params = {"ark": self.ark}
-#             if date:
-#                 params["date"] = date
-#             handler = XmlParser if raw else Issues.from_xml
-#             return sync_get(routes.issues, handler, params)
-#         raise ValueError("Service Issues requires an ARK with the qualifier `date`.")
-
-#     def oairecord(self, raw=False):
-#         params = {"ark": self.ark.short()}
-#         handler = XmlParser
-#         return sync_get(routes.oairecord, handler, params)
-
-#     def pagination(self, raw=False):
-#         params = {"ark": self.ark.short()}
-#         handler = XmlParser if raw else Pagination.from_xml
-#         return sync_get(routes.pagination, handler, params)
-
-#     def image(self, page: int, qualifier: str = "thumbnail"):
-#         # Only 4 values are valid for the image resolution selector
-#         valid_resolutions = ["thumbnail", "lowres", "medres", "highres"]
-#         if qualifier not in valid_resolutions:
-#             raise ValueError(f"{qualifier} must be one of {valid_resolutions}")
-
-#         url = routes.image(self.ark, page, qualifier)
-#         return sync_get(url, lambda c: Image.open(BytesIO(c)))
-
-#     def contentsearch(
-#         self,
-#         query: str,
-#         page: int = None,
-#         start_result: int = 1,
-#         index_only=False,
-#         raw=False,
-#     ):
-#         params = {"ark": self.ark.short(), "query": query}
-#         if not index_only or page:
-#             params.update({"startResult": start_result, "page": page})
-
-#         handler = XmlParser if raw else ContentSearch.from_xml
-#         return sync_get(routes.contentsearch, handler, params)
-
-#     def toc(self, raw=False):
-#         params = {"ark": self.ark}
-#         return sync_get(routes.toc, Toc.from_xml, params)
-
-#     def textebrut(self):
-#         url = routes.textebrut(self.ark)
-#         handler = lambda c: BeautifulSoup(c, features="lxml")
-#         return sync_get(url, handler)
-
-#     def requestdigitalelement(self, deb: int, e: str = "ALTO"):
-#         params = {"O": self.ark.short(), "Deb": deb, "E": e}
-#         return sync_get(routes.requestdigitalelement, XmlParser, params)
-
-
-# class Toc:
-
-#     __wrap_tag = "TEI.2"
-
-#     @classmethod
-#     def from_xml(cls, raw_xml):
-#         return XmlParser(raw_xml).find(Toc.__wrap_tag)
-
-
-# class Pagination:
-#     @classmethod
-#     def from_xml(cls, raw_xml):
-#         parser = PaginationParser(raw_xml)
-#         return {
-#             "structure": parser.structure(),
-#             "page_list": list(parser.page_list()),
-#         }
-
-
-# class OAIRecord:
-#     __fields = [
-#         "visibility_rights",
-#         "title",
-#         "typedoc",
-#         "provenance",
-#         "dewey",
-#         "sdewey",
-#         "nqamoyen",
-#         "date",
-#         "mode_indexation",
-#         "source",
-#         "first_indexation_date",
-#     ]
-
-#     @classmethod
-#     def from_xml(cls, raw_xml):
-#         parser = OAIRecordParser(raw_xml)
-#         fields_dict = {field: getattr(parser, field)() for field in cls.__fields}
-#         record = {
-#             "metadata": parser.metadata(),
-#             "notice": parser.notice(),
-#             "_parser": parser,
-#         }
-#         return {**fields_dict, **record}
-
-
-# class ContentSearch:
-#     @classmethod
-#     def from_xml(cls, raw_xml):
-#         parser = ContentSearchParser(raw_xml)
-#         return {
-#             "metadata": parser.metadata(),
-#             "items": list(parser.items()),
-#             "query": parser.query(),
-#             "_parser": parser,
-#         }
-
-
-# class Issues:
-#     @classmethod
-#     def from_xml(cls, raw_xml):
-#         parser = IssuesParser(raw_xml)
-#         return {
-#             "metadata": parser.metadata(),
-#             "year_list": parser.year_list(),
-#             "issue_list": parser.issue_list(),
-#             "_parser": parser,
-#         }
